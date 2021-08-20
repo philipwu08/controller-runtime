7eed492dc159318aba9edd681d1a55a3f00d68b6
Philip Wu <81585886+philipwu08@users.noreply.github.com>
2021-08-19 23:41:54 -0400

    Update error msg

--- pkg/internal/controller/controller.go
+++ pkg/internal/controller/controller.go
@@ -18,7 +18,6 @@ package controller
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	"sync"
 	"time"
@@ -137,7 +136,7 @@ func (c *Controller) Start(ctx context.Context) error {
 	// but lock outside to get proper handling of the queue shutdown
 	c.mu.Lock()
 	if c.Started {
-		return errors.New("controller was started more than once. This is likely to be caused by being added to a manager multiple times")
+		return fmt.Errorf("controller:%v was started more than once. This is likely to be caused by being added to a manager multiple times", c.Name)
 	}
 
 	c.initMetrics()
